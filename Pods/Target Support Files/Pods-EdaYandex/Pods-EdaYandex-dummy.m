223e55e547b1d266a7b9174e847da3ac0a94d37a
Mac <denpef@gmail.com>
2018-09-26 17:57:03 +0300

    add pods folder

---
+++ Pods/Target Support Files/Pods-EdaYandex/Pods-EdaYandex-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EdaYandex : NSObject
+@end
+@implementation PodsDummy_Pods_EdaYandex
+@end
---
+++ Pods/Target Support Files/Pods-EdaYandexUITests/Pods-EdaYandexUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EdaYandexUITests : NSObject
+@end
+@implementation PodsDummy_Pods_EdaYandexUITests
+@end
